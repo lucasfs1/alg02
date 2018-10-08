2b20f9b1ffb4fb1d0cbe3713a62a704b4124c692
lucasfs1 <43751211+lucasfs1@users.noreply.github.com>
2018-10-08 09:21:21 -0300

    comentario e biblioteca

--- Aulas/ConsoleApplication1/ConsoleApplication1.cpp
+++ Aulas/ConsoleApplication1/ConsoleApplication1.cpp
@@ -1,11 +1,17 @@
 // Aula08/10/2018.cpp : Defines the entry point for the console application.
 //comentarios
+/*
+comentario longo 
+(mais de uma linha)
 
+*/
 
-#include "stdafx.h"
+#include "stdafx.h"                                                 // incluso de biblioteca
+                                                                   //C ansi
+#include <stdio.h>                                                // padrao de entrada e saida
+#include <stdlib.h>                                              //biblioteca padrao onde tem comandos
 
-
-int main()
+int main()                                                     // ponto de inicio
 {
     return 0;
 }
