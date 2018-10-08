aa317179ea508c4811c835f4be2ca28e3ecbaf3b
lucasfs1 <43751211+lucasfs1@users.noreply.github.com>
2018-10-08 11:20:47 -0300

    string e exemplos

--- Aulas/ConsoleApplication1/ConsoleApplication1.cpp
+++ Aulas/ConsoleApplication1/ConsoleApplication1.cpp
@@ -17,15 +17,59 @@ float b; //real(ponto flutuante)
 double c; //real(maior precisao)
 bool d; //booleano (verdadeiro/falso) (true/false)
 
+//compliar:ctrl+shift+b
+//n=pular l.
+//t=espaço
 
-
-
-int main() // ponto de inicio
+int main() // ponto de inicio f5 executa
 {
      //variavel local	
 	//()visivel somente nesta funcao,a partir dessa linha)
 
 	int e;
+   
+
+	printf("Hello world!\n");
+
+	//como imprimri valores
+	printf("\n");
+	printf("%i", 99 );
+		printf("\n");
+		printf("%f\n",100.01);
+
+//texto (string) e valores juntos
+		printf("Aqui vai o valor %i,total da soma.\n", 98 + 10);
+
+		printf("%i + %i = %i\n", 500, 600, 500 + 600);
+
+		//////////////////////////////////////////////////////////////////
+
+		a = 150;
+		e = 270;
+
+		int f = a + e;
+		printf("o resultado da soma eh:%i\n", f);
+
+		////////////////////////////////////////////////////////////////////
+  
+		//leitura SCANF / SCANF_S
+
+		scanf_s("%i", &f);
+
+		printf("\nVoce digitou %i\n", f); // verificacao
+
+///////////////////////////////////////////////////////////////////////////////
+		printf("Digite um valor real(float):");
+
+		scanf_s("%f", &b);
+
+		printf("\nVoce digitou %f\n", b);
+
+
+
+
+		system("pause");
+
 
 	return 0;
 }
