c336a5c2316157cf2211163d5b34833cbe012765
lucasfs1 <43751211+lucasfs1@users.noreply.github.com>
2018-10-10 10:21:30 -0300

    soma2valores

--- Aulas/aula 10-10/aula 10-10.cpp
+++ Aulas/aula 10-10/aula 10-10.cpp
@@ -2,14 +2,63 @@
 //
 
 #include "stdafx.h"
+#include <stdlib.h>
+void HelloWorld()
+{
+	printf("Hello Word!\n");
+} 
 
+int Soma2Valores(int a, int b)
+{
+
+	int c;
+	c = a + b;
+	return c;
+}
 
 int main()
 {
+	HelloWorld();
+	int d = Soma2Valores(99, 1);
+	printf("\tResultado: %i\n", d);
+
+
+
+
+
+		system("pause");
     return 0;
 }
 
-//void identifica o  retorno 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//Debug acompanha a execuçao passo a passo
+//f5-executa debug
+//F9-break points
+//f10-executa sem mostrar(passo a passo)
+//f11-executa passo a passo 
+//void identifica o  retorno e nao tem retorno
 //soma de 2 valores (int a,int b)
 //int c 
 //c=a+b
@@ -18,3 +67,4 @@ int main()
 //pode-se usar tambem (return a + b )
 //ex:int d =soma de 2 valores (3,7)
 //funçoes devem ser escritas antes do (main)
+//
