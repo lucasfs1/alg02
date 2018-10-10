a30a00e79238f4c4bac57032ed5b2e411ad79492
lucasfs1 <43751211+lucasfs1@users.noreply.github.com>
2018-10-10 08:49:29 -0300

    aula 10/10 inicio

---
+++ aula 10-10/aula 10-10/aula 10-10.cpp
@@ -0,0 +1,20 @@
+// aula 10-10.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+
+
+int main()
+{
+    return 0;
+}
+
+//void identifica o  retorno 
+//soma de 2 valores (int a,int b)
+//int c 
+//c=a+b
+// return c
+//usa-se (soma de 2 valores (3,7) ) para chamar a funçao
+//pode-se usar tambem (return a + b )
+//ex:int d =soma de 2 valores (3,7)
+//funçoes devem ser escritas antes do (main)
